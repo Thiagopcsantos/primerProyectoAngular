0961df3321a8de25de3c6c6330df7fdf643271f6
Thiago <thiagopcsantosmail.com>
2020-04-06 21:43:21 -0400

    first commit angular

---
+++ holaMundo/src/app/titulos/titulos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulos',
+  templateUrl: './titulos.component.html',
+  styleUrls: ['./titulos.component.scss']
+})
+export class TitulosComponent implements OnInit {
+  nombre:string = "Maria"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
