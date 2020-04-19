3e8c7b94a681140632a295ebd940c93670019544
Thiago <thiagopcsantosmail.com>
2020-04-18 20:38:00 -0400

    commit sabado

--- holaMundo/src/app/directiva/directiva.component.ts
+++ holaMundo/src/app/directiva/directiva.component.ts
@@ -5,6 +5,7 @@ interface Productos{
   stock: number;
   fabricante: string;
   fechaVence: Date;
+  esImportante: boolean;
 }
 
 @Component({
@@ -23,24 +24,32 @@ productos: Array<Productos> = [
     nombre : 'Arroz',
     stock : 20,
     fabricante : 'Camil',
-    fechaVence : new Date('04/15/2020')
+    fechaVence : new Date('04/15/2020'),
+    esImportante : true
   },
   {
     nombre : 'Feijao',
     stock : 12,
     fabricante : 'Super Caldo',
-    fechaVence : new Date('08/19/2020')
+    fechaVence : new Date('08/19/2020'),
+    esImportante : false
+
   },
   {
     nombre : 'Farofa',
     stock : 100,
     fabricante : 'YOKI',
-    fechaVence : new Date('09/12/2024')
+    fechaVence : new Date('09/12/2024'),
+    esImportante : true
   }
 ]
 
 
   cargando: boolean = true;
+
+mostrarCuadrado:boolean = false;
+
+
 
   constructor() { }
 
@@ -58,6 +67,9 @@ productos: Array<Productos> = [
 cambiarPestana(pestana:string) {
   this.pestana = pestana;
 }
-  
+
+alternarFondo(){
+  this.mostrarCuadrado = !this.mostrarCuadrado
+}
 
 }
